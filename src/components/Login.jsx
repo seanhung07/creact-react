4e3b17a152579c4286d733883b5dd6c11c85e62f
Sean Hung <sean80111108@gmail.com>
2020-02-13 12:43:06 +0800

    again

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { GoogleLogin } from "react-google-login";
 import "../assets/App.css";
@@ -10,20 +9,6 @@ const Login = () => {
     if (!isNaN(response.tokenId)) {
       window.localStorage.setItem("tokenid", response.tokenId);
       window.location.replace("/dashboard");
-=======
-
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-import '../assets/App.css';
-import Logo from '../assets/logo.png';
-
-const Login = () => {
-  function responseGoogle(response) {
-    console.log(response);
-    if (!isNaN(response.googleId)) {
-      window.localStorage.setItem('token', response.googleId);
-      window.location.replace("/documents");
->>>>>>> 12a30dd4eb20c3a1e5051798c389ec21e621fcbe
     }
   }
 
@@ -39,23 +24,13 @@ const Login = () => {
         <GoogleLogin
           clientId="1015132118515-s5vvq9n86s6pabsvj8fkk6ic2d5iuuak.apps.googleusercontent.com"
           buttonText="Sign in With Google"
-<<<<<<< HEAD
           onSuccess={response => responseGoogle(response)}
           onFailure={response => responseGoogle(response)}
           cookiePolicy={"single_host_origin"}
-=======
-          onSuccess={(response) => responseGoogle(response)}
-          onFailure={(response) => responseGoogle(response)}
-          cookiePolicy={'single_host_origin'}
->>>>>>> 12a30dd4eb20c3a1e5051798c389ec21e621fcbe
         />
       </header>
     </div>
   );
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 12a30dd4eb20c3a1e5051798c389ec21e621fcbe
 
 export default Login;
